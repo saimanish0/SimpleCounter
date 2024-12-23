16001a890a733b9f9ab158a3b747ee0f6abf5fff
saimanish0 <psaimanish23@outlook.com>
2024-12-23 16:48:24 +0530

    Fully Funtional Simple Counter App

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
-import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button} from 'react-native-paper';
 import React, {useState} from 'react';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
   const handleDecrement = () => {
     if (count > 0) {
       setCount(count - 1);
-    } else {
-      Alert.alert('Warning', 'Cannot decrement below 0!');
     }
+  };
+
+  const handleMultiply = () => {
+    setCount(count * 2);
+  };
+
+  const handleDivison = () => {
+    setCount(count / 2);
+  };
+
+  const handleReset = () => {
+    setCount(0);
   };
 
   return (
@@ -17,13 +31,21 @@ export default function App() {
       <Text style={styles.title}>Simple Counter App</Text>
       <Text style={styles.counter}>{count}</Text>
       <View style={styles.buttonContainer}>
-        <Button
-          title="Increment"
-          onPress={() => {
-            setCount(count + 1);
-          }}
-        />
-        <Button title="Decrement" onPress={() => handleDecrement()} />
+        <Button mode="elevated" onPress={handleIncrement}>
+          Increment
+        </Button>
+        <Button mode="elevated" onPress={handleDecrement}>
+          Decrement
+        </Button>
+        <Button mode="elevated" onPress={handleMultiply}>
+          Multiply x 2
+        </Button>
+        <Button mode="elevated" onPress={handleDivison}>
+          Divide / 2
+        </Button>
+        <Button mode="contained" onPress={handleReset}>
+          Reset
+        </Button>
       </View>
     </View>
   );
@@ -47,8 +69,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   buttonContainer: {
+    backgroundColor: '#e5e5e5b6',
+    borderRadius: 16,
+    padding: 24,
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '60%',
+    justifyContent: 'space-evenly',
+    flexWrap: 'wrap',
+    width: '90%',
+    rowGap: 16,
+    columnGap: 12,
   },
 });
